bbc286fd772069cfd7ffbcd926d36b57d0425988
Killian Cch <killian.cch@gmail.com>
2023-03-15 13:26:25 +0000

    Add Entity Pokemon, Type and relation

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -1,43 +1,63 @@
 <?php
-
+// src/Command/CreateUserCommand.php
 namespace App\Command;
 
-use Symfony\Component\Console\Attribute\AsCommand;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Console\Question\Question;
+use App\Entity\User;
 
-#[AsCommand(
-    name: 'app:create-user',
-    description: 'Add a short description for your command',
-)]
-class CreateUserCommand extends Command
-{
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+class CreateUserCommand extends Command {
+    protected static $defaultName = 'app:create-user';
+
+    protected static $defaultDescription = "Create the first User (admin). If a user already exists in DB, nothing happens";
+
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher) {
+        $this->em = $entityManager;
+        $this->hasher = $hasher;
+
+        parent::__construct();
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+    protected function execute(InputInterface $input, OutputInterface $output) {
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $helper = $this->getHelper('question');
+        $questionLogin = new Question("username ? ");
+        $questionPassword = new Question("password ? ");
+        $questionPassword->setHidden(true);
+        $questionPassword->setHiddenFallback(false);
+
+        $login = $helper->ask($input, $output, $questionLogin);
+        $password = $helper->ask($input, $output, $questionPassword);
+
+        $output->writeln("Username : " . $login);
+        $output->writeln("Password : " . $password);
+
+        // No user must be in DB
+        $users = $this->em->getRepository(user::class)->findAll();
+        if ($users) {
+            $output->writeln(count($users) . 'user(s) in DB. No creation allowed');
+            return Command::FAILURE;
         }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        $user = new User();
+        $user->setEmail($login);
+        $user->setPassword($password);
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $hash = $this->hasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hash);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln("Success !");
+        return Command::SUCCESS;
 
         return Command::SUCCESS;
     }
